37681519753bcea69de2e7b403f4c3253320658d
jeongsooyoung <code.ameba@gmail.com>
2019-08-16 15:55:31 +0900

    get movies

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import axios from 'axios';
-import { async } from 'q';
 
+const movieList = 'https://yts.lt/api/v2/list_movies.json';
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
   getMovies = async () => {
-    const movies = await axios.get('https://yts.lt/api/v2/list_movies.json');  
+    const movies = await axios.get(movieList);  
+    this.setState({ movies });
+    console.log(movies);
+    setTimeout(this.setState({ isLoading: false }), 3000);
   }
-  acomponentDidMount() {
+  componentDidMount() {
     this.getMovies();
+    console.log('get movies');
   }
+
   render() {
     const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We Are Ready!'}</div>;
+    return (
+      <div>{isLoading ? 'Loading...' : 'We Are Ready!'}</div>
+    );
   }
 }
 
